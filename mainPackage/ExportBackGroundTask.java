da20fe1348336776218ba5084585b68ff8b3904f
ccha504 <ccha504@aucklanduni.ac.nz>
2014-10-22 19:38:13 +1300

    did export bar

--- mainPackage/ExportBackGroundTask.java
+++ mainPackage/ExportBackGroundTask.java
@@ -1,7 +1,9 @@
 package mainPackage;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
@@ -28,31 +30,32 @@ public class ExportBackGroundTask extends SwingWorker<Integer, String> {
 	private ProcessBuilder builder;
 
 	private final JProgressBar progressBar = new JProgressBar(0, 100);
-	private String percentageCompleted;
+;
 
 	//Set the export frame 
 	private JFrame exportFrame = new JFrame("Exporting...");
-	private JPanel exportPanel = new JPanel(new BorderLayout());
+	private JPanel exportPanel = new JPanel();
 
 	private JLabel exportLabel = new JLabel("Export is in progress....");
-	
-	
+	private JLabel separator= new JLabel("");
+
 	protected ExportBackGroundTask(String cmd) {
 		this.cmd = cmd;
-
-
-
+		
+		exportLabel.setFont(new Font("TimesRoman", Font.BOLD, 12));
+		//Create the progress bar
+		progressBar.setPreferredSize(new Dimension(220, 20));
+		separator.setPreferredSize(new Dimension(220, 5));
+		progressBar.setValue(0);
+		exportPanel.add(separator);
 		exportPanel.add(exportLabel);
 		exportPanel.add(progressBar);
 		exportFrame.setContentPane(exportPanel);
-		exportFrame.setLocation(450, 100);
+		exportFrame.setLocation(700, 400);
 		exportFrame.setResizable(false);
-		exportFrame.setSize(800, 650);
+		exportFrame.setSize(250,100);
 		exportFrame.setVisible(true);
-		//Create the progress bar
-		progressBar.setStringPainted(true);
-		progressBar.setPreferredSize(new Dimension(20, 20));
-		progressBar.setValue(0);
+
 
 	}
 
@@ -93,18 +96,14 @@ public class ExportBackGroundTask extends SwingWorker<Integer, String> {
 	@Override
 	protected void process(List<String> chunks) {
 		if (!isCancelled()) {
-			for (String s : chunks) {
-				Pattern p = Pattern.compile("(\\d*)\\%");
-				Matcher m = p.matcher(s);
 
-				while (m.find()) {
-					percentageCompleted = m.group(1);
-					progressBar.setValue(Integer.parseInt(percentageCompleted));
-				}
+			progressBar.setIndeterminate(true);
 
-			}
+
+
 		}
 	}
+
 
 	protected void done(){
 
